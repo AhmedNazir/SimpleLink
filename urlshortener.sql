7802f6b482171721ba88b98fd094e4e39151cc6f
Ahmed Nazir <Ahmednazir333@gmail.com>
2021-01-03 16:24:25 +0600

    update

---
+++ urlshortener.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 03, 2021 at 11:23 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `urlshortener`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `urls`
+--
+
+CREATE TABLE `urls` (
+  `shorturl` varchar(16) NOT NULL,
+  `longurl` varchar(512) NOT NULL,
+  `userid` varchar(20) NOT NULL DEFAULT 'guest',
+  `creator` varchar(32) NOT NULL,
+  `edit` varchar(8) NOT NULL,
+  `preview` tinyint(1) NOT NULL DEFAULT 0,
+  `capcha` tinyint(1) NOT NULL DEFAULT 0,
+  `passcode` varchar(8) DEFAULT NULL,
+  `submission` date NOT NULL,
+  `expire` date DEFAULT NULL,
+  `click` int(16) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(60) NOT NULL,
+  `useremail` varchar(60) NOT NULL,
+  `userid` varchar(60) NOT NULL,
+  `userpassword` varchar(60) NOT NULL,
+  `usertype` varchar(60) NOT NULL,
+  `totalurl` int(16) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `urls`
+--
+ALTER TABLE `urls`
+  ADD UNIQUE KEY `shorturl` (`shorturl`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD UNIQUE KEY `userid` (`userid`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
